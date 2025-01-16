d72a097784e90d6a7a4963e77dafdcfcce54d96f
Hamzaalvi12 <hamzaalvi754@gmail.com>
2025-01-16 23:12:53 +0500

    Revision time--Full

---
+++ Revision/35 days of code/D-0.py
@@ -0,0 +1,18 @@
+#Adding 2 numbers
+a = 4
+b = 3
+
+print("Value of A is: ",a)
+print("Value of B is:",b)
+#ADD
+print("Add: ",a+b)
+
+#Minus
+print("Minus: ",a-b)
+
+#Multiply
+print("Multiply",a*b)
+
+#Devide
+print("Devide: ",a/b)
+
